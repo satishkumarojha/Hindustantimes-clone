904504aa23003f50977284dd8b3e6f9ff9f00580
Prashant-KT <tiwariprashant0395@gmail.com>
2022-05-07 21:40:01 +0530

    footerandweather

--- backend/src/configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDb = ()=>{
-    return mongoose.connect("mongodb+srv://hindustantimes:123hindustan@cluster0.yy8fs.mongodb.net/hindustantimes");
-}
-
-module.exports=connectDb;
-
